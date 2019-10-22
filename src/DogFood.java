c87dba60f686d3628671649ac468ae2b96d8c116
BuildTools <unconfigured@null.spigotmc.org>
2019-10-22 00:30:14 -0500

    Completely DOne

--- src/DogFood.java
+++ src/DogFood.java
@@ -17,14 +17,63 @@ public class DogFood
    public DogFood(String line)
    {
 		Scanner chopper = new Scanner(line);
+		String input = chopper.nextLine();
+		String[] numbers = input.split(" ");
+		int[] finalNum = new int[numbers.length];
+		for(int i = 0; i < numbers.length; i++)
+		{
+			finalNum[i] = Integer.parseInt(numbers[i]);
+		}
 		double cups = 0;
+		for(int Num: finalNum)
+		{
+			cups += getAmount(Num)*7;
+		}
+		cups = cups/60;
+		double bags = Math.ceil(cups);
+		System.out.println(bags);
+		int finalBags = (int)bags;
+		System.out.println(finalBags + " - 10 POUND BAGS");
 
+		
 		//calc code goes here
 
    }
 
-   public double getAmount()
+   public double getAmount(int weight)
    {
+	if(weight>=80)
+	{
+		return 7.5;
+	}
+	else if(weight >= 60)
+	{
+		return 6.0;
+	}
+	else if(weight >= 40)
+	{
+		return 4.5;
+	}
+	else if(weight >= 20)
+	{
+		return 3.5;
+	}
+	else if(weight >= 10)
+	{
+		return 2.0;
+	}
+	else if(weight >= 8)
+	{
+		return 1.5;
+	}
+	else if(weight >= 5)
+	{
+		return 1.0;
+	}
+	else if(weight >= 2)
+	{
+		return 0.5;
+	}
    	return 0;
    }
 
--- src/DogFoodRunner.java
+++ src/DogFoodRunner.java
@@ -4,11 +4,25 @@
 
 import java.util.Scanner;
 import static java.lang.System.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.lang.Math;
 
 public class DogFoodRunner
 {
 	public static void main(String[] args)
 	{
+		try {
+			File FileInput = new File("DogFood.txt");
+			Scanner input = new Scanner(FileInput);
+			while(input.hasNextLine())
+			{
+				DogFood a = new DogFood(input.nextLine());
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
